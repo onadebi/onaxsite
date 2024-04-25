d70b1814f9cf5850dfcece9144d788d9d8c0b4df
Onadebi <onadebi@onaxsys.com>
2024-04-24 22:37:40 -0400

    Use generic views: Less code is better

--- onaxsite/poll/views.py
+++ onaxsite/poll/views.py
@@ -2,31 +2,50 @@ from django.shortcuts import render, get_object_or_404;
 from django.db.models import F;
 from django.urls import reverse;
 from .models import Choice, Question;
+from django.views import generic as gene;
 from django.http import HttpResponse, Http404, HttpRequest, HttpResponseRedirect;
 
 # Create your views here.
-def index(request: HttpRequest):
-    # http://127.0.0.1:8000/home/?name=onadebi
-    latest_questions = Question.objects.order_by("-pub_date")[:5];
-    # output =",".join([q.question_text for q in latest_questions]);
-    context = {"latest_question_list": latest_questions};
-    return render(request, "poll/index.html", context)
+class IndexView(gene.ListView):
+    template_name = "poll/index.html"
+    context_object_name = "latest_question_list";
 
-def detail(request, question_id: int):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    #region Alternative to the above
-    question = get_object_or_404(Question, pk=question_id)
-    #endregion
-    return render(request, "poll/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5];
+
+class DetailView(gene.DetailView):
+    model = Question
+    template_name = "poll/detail.html"
 
 
-def results(request: HttpRequest, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "poll/results.html", {"question": question}, status=201)
+class ResultsView(gene.DetailView):
+    model = Question
+    template_name = "poll/results.html"
 
+#region OLD Views code
+# def index(request: HttpRequest):
+#     # http://127.0.0.1:8000/home/?name=onadebi
+#     latest_questions = Question.objects.order_by("-pub_date")[:5];
+#     # output =",".join([q.question_text for q in latest_questions]);
+#     context = {"latest_question_list": latest_questions};
+#     return render(request, "poll/index.html", context)
+
+# def detail(request, question_id: int):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     #region Alternative to the above
+#     question = get_object_or_404(Question, pk=question_id)
+#     #endregion
+#     return render(request, "poll/detail.html", {"question": question})
+
+
+# def results(request: HttpRequest, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "poll/results.html", {"question": question}, status=201)
+#endregion
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
