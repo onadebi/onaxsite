373bd2ea7d9c7eb88f1cd99d141020862d19f31a
Onadebi <onadebi@onaxsys.com>
2024-04-25 12:44:00 -0400

    Clean up

--- onaxsite/onaxmain/views.py
+++ onaxsite/onaxmain/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse, HttpRequest;
 # Create your views here.
 def index(request: HttpRequest):
     # http://127.0.0.1:8000/home/?name=onadebi
-    return HttpResponse(f"This site is currently under construction. {request.GET.get('name', 'No name provided')}")
+    # return HttpResponse(f"This site is currently under construction. {request.GET.get('name', 'No name provided')}")
+    return render(request, "onaxmain/index.html", {'title': 'Onadebi\'s Home Page'})
 
+def about(request: HttpRequest):
+    return render(request, "onaxmain/about.html", {'title': 'Onadebi\'s About Page'})
 
 def portfolio(request: HttpRequest):
-    return HttpResponse("templates/portfolio.html", {'title': 'Portfolio'})
+    return render(request,"onaxmain/portfolio.html", {'title': 'Portfolio'})
