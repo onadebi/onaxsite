640bd69fa36d02f565f73f96db8f8dd888111ec7
Onadebi <onadebi@onaxsys.com>
2024-04-26 05:40:32 -0400

    Implementation of base layout for pages

--- onaxsite/onaxmain/urls_controller.py
+++ onaxsite/onaxmain/urls_controller.py
@@ -4,6 +4,6 @@ from . import views;
 app_name = "onaxmain";
 urlpatterns = [
     path("",views.index, name="index"),
-    path("portfolio",views.portfolio, name="portfolio"),
+    path("portfolio",views.portfolio, name="portfolioz"),
     path("about",views.about, name="about"),
 ]
--- onaxsite/onaxmain/views.py
+++ onaxsite/onaxmain/views.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse, HttpRequest;
 def index(request: HttpRequest):
     # http://127.0.0.1:8000/home/?name=onadebi
     # return HttpResponse(f"This site is currently under construction. {request.GET.get('name', 'No name provided')}")
-    return render(request, "onaxmain/index.html", {'title': 'Onadebi\'s Home Page'})
+    context: dict = {
+        'description': 'Trust us to bring your digital expectations to life.',
+        'title': 'Onaxsys Media',
+    };
+    return render(request, "onaxmain/index.html",context)
 
 def about(request: HttpRequest):
     return render(request, "onaxmain/about.html", {'title': 'Onadebi\'s About Page'})
